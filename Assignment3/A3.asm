d41b2f4f2adea3500eab35249f438ae70cbbcd2b
Atharva Joshi <athu200299@gmail.com>
2019-02-06 18:39:58 +0530

    Update A3.asm
    Completed Assignment 3

--- Assignment3/A3.asm
+++ Assignment3/A3.asm
@@ -7,13 +7,26 @@ print	MACRO	msg
 	int	21h
 	endm
 	
+input	MACRO	var
+	mov	ax,0
+	mov	ah,01h
+	int	21h
+	mov	var,al	
+	endm
+	
 	.data
-buff	db	25;max size
+buff	db	26;max size
 	db	?;input size
 	db	25 DUP(0);Actual String Stored here
+n	db	0
 nl	db	10,13,'$'
 menu	db	'Enter string:$'
-	
+menu1	db	10,13,'String Operations',10,13,'1)String Length',10,13,'2)Reverse String',10,13,'3)Check Palindrome',10,13,'4)Exit',10,13,'Enter Choice:$'
+menu2	db	'Enter Valid Choice',10,13,'$'
+p1	db	'Reverse is:$'
+p2	db	'String is not a palindrome$'
+p3	db	'String is a Palindrome$'
+choice	db	0	
 	.code
 main	proc	far
 	mov	ax,@data
@@ -24,6 +37,7 @@ main	proc	far
 	mov	dx, offset buff
 	int	21h
 	
+	;Adding teminating character
 	mov	si,offset buff + 1
 	mov	cl, [ si ]
 	mov	ch,0
@@ -33,11 +47,111 @@ main	proc	far
 	mov	[ si ],al
 	
 	print	nl
-	mov	ah,9
-	mov	dx,offset buff + 2
+	
+start:	print	menu1
+	input	choice
+	print 	nl
+	mov	ax,00h
+	mov	bx,00h
+	
+	mov	ax,31h
+	mov	bl,choice
+	cmp	ax,bx
+	jz	call1
+	
+	mov	ax,32h
+	cmp	ax,bx
+	jz  	call2
+	
+	mov	ax,33h
+	cmp	ax,bx
+	jnz	n3
+	jmp	call3
+	
+n3:	mov	ax,34h
+	cmp	ax,bx
+	jnz	erro
+	jmp	exit
+	
+erro:	print	menu2
+	jmp	start
+	
+	
+	
+call1:	mov	ax,0
+	mov	al,[buff + 1]
+	mov	bx,10
+	div	bl
+	
+	mov	dx,0
+	mov	dl,al
+	mov	bx,0
+	mov	bl,ah
+	
+	add	bl,30h
+	add	dl,30h
+	
+	mov	ah,02h
 	int	21h
 	
-	mov	ah,4ch
+
+	mov	dl,bl
+	int	21h
+	print	nl
+	mov	ax,0
+	mov	bx,0
+	mov	dx,0
+	jmp	start
+
+
+
+call2:	mov	si,[offset buff + 2]
+	mov	ax,0
+	mov	al,[buff + 1]
+	add	si,ax
+	mov	cx,1
+	print	nl
+	dec	si
+	print	p1
+	mov	ax,0
+rev:	
+	mov	ah,02h
+	mov	dx,0
+	mov	dl,[si]
+	int	21h	
+	dec	si
+	cmp	si,cx
+	jnz	rev
+	print	nl
+	jmp	start
+	
+call3:	mov	si,[offset buff + 2]
+	mov	ax,0
+	mov	al,[buff + 1]
+	add	si,ax
+	mov	cx,si
+	dec	si
+	mov	di,2
+	
+pal:
+	cmp	di,cx
+	jz	cont2
+	mov	ax,[si]
+	mov	bx,[di]
+	cmp	al,bl
+	jnz	cont1
+	inc	di
+	dec	si
+	jmp	pal
+	
+cont1:	print	p2
+	jmp	leav
+cont2:	print	p3
+		
+leav:	print	nl
+	jmp	start	
+	
+exit:	mov	ah,4ch
 	int	21h
 	
 	main	endp
