e8cdebb134034303a82fb87dbfa7842020c624bd
Atharva Joshi <athu200299@gmail.com>
2019-02-26 20:57:53 +0530

    Update A4.asm

--- Assignment4/A4.asm
+++ Assignment4/A4.asm
@@ -15,11 +15,31 @@ input	MACRO	var
 	mov	var,al	
 	endm
 	
-
+getstr	MACRO	buff
+	mov	ah,0Ah
+	mov	dx, offset buff
+	int	21h
 	
-	.data
-
+	;Adding teminating character
+	mov	si,offset buff + 1
+	mov	cl, [ si ]
+	mov	ch,0
+	inc	cx
+	add	si,cx
+	mov	al,'$'
+	mov	[ si ],al
+	endm
+	
+	.data 
+buff1	db	26;max size
+	db	?;input size
+	db	25 DUP(0);Actual String Stored here
+buff2	db	26;max size
+	db	?;input size
+	db	25 DUP(0);
 nl	db	10,13,'$'
+menu1	db	'Enter String 1:$'	
+menu2	db	'Enter String 2:$'
 menu3	db	'Press 1 for Concat of Strings',10,13,'Press 2 for String Compare',10,13,'Press 3 for',10,13,'Press 4 to Exit',10,13,'Enter Choice:$'
 choice	db	0
 menu4	db	'Enter Valid Choice$'
@@ -29,14 +49,22 @@ main	proc	far
 	mov	ax,@data
 	mov	ds,ax
 	
+	print	menu1	
+	getstr	buff1
+	
+	print	nl
+	
+	print	menu2
+	getstr	buff2
+	
+	print	nl
 	
 again:	print	nl
 	print	nl
 	print	menu3
 	input	choice
 	print	nl
-
-		
+	
 	mov	ax,0
 	mov	al,choice
 	mov 	bl,31h
@@ -57,12 +85,8 @@ again:	print	nl
 	jmp	call5
 	
 call1:
-	extrn	concat:far
-	call	concat
 	jmp	again
 call2:
-	extrn	cmpare:far
-	call	cmpare
 	jmp	again
 call3:
 	jmp	again
