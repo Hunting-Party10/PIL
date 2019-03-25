403f999ed621ad889fb1755e33863610a9d3c828
Atharva Joshi <athu200299@gmail.com>
2019-03-25 21:21:53 +0530

    Assignment 6,7

---
+++ Assignment7/external.asm
@@ -0,0 +1,9 @@
+org 00h
+MOV R0,#40h
+MOV dptr,#60h
+mov a,#10
+mov @R0,a
+mov a,@R0
+movx @dptr,a
+
+End
---
+++ Assignment7/internal.asm
@@ -0,0 +1,10 @@
+org 00h
+MOV R0,#40h
+MOV R1,#60h
+Mov @R0,#20h
+
+
+Mov a,@R0
+Mov @R1,a
+
+End
